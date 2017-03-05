13bdfb1f8b1550d8ad578d158efbb5a17547ed6c
Michail Kurockin <Michail.Kurachkin@promwad.com>
2017-03-05 15:50:05 +0300

    conf: move config into /etc/sr90_automation/ directory

--- src/conf/conf.go
+++ src/conf/conf.go
@@ -6,11 +6,14 @@ import (
     "github.com/BurntSushi/toml"
 )
 
+const CONFIG_FILE = "/etc/sr90_automation/module_io.conf"
+
 type Module_io_cfg struct {
 	Uart_dev string
 	Uart_speed string
 	Responce_timeout int
 	Repeate_count int
+	Exec_path string
 	Exec_script string
 	Control_socket string
 }
@@ -19,14 +22,14 @@ func Conf_parse() (*Module_io_cfg, error) {
 	var err error
 	var conf Module_io_cfg
 	
-	config_text, err := ioutil.ReadFile("/etc/module_io.conf")
+	config_text, err := ioutil.ReadFile(CONFIG_FILE)
 	if err != nil {
-		return nil, fmt.Errorf("Can't open config file /etc/module_io.conf: %v", err)
+		return nil, fmt.Errorf("Can't open config file %s: %v", CONFIG_FILE, err)
 	}
 
 	_, err = toml.Decode(string(config_text), &conf)
 	if err != nil {
-		return nil, fmt.Errorf("Can't decode config file /etc/module_io.conf: %v", err)
+		return nil, fmt.Errorf("Can't decode config file %s: %v", CONFIG_FILE, err)
 	}
 	
 	return &conf, nil
